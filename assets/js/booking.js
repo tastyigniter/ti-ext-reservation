7e918ccc9baabf4b7057d1ff3178757ab77648ae
Ryan Mitchell <ryan@thoughtcollective.com>
2020-10-20 11:28:43 +0100

    Visual indicator of loading progress

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -10,22 +10,22 @@
         this.$guestPickerValue = 1
         this.$locationPicker = null
         this.$locationPickerValue = 1
-        
+
         this.init()
     }
 
     Booking.prototype.init = function () {
 	    if (this.$datePicker = this.$el.find('[data-control="datepicker"]'))
             this.initDatePicker();
-            
+
 	    if (this.$guestPicker = this.$el.find('[name="guest"]'))
             this.initGuestPicker();
-            
+
         if (this.$locationPicker = this.$el.find('[name="location"]'))
             this.initLocationPicker();
-        
+
     }
-    
+
     Booking.prototype.initDatePicker = function () {
         this.$datePickerValue = this.options.datepickerStartdate;
         this.$datePicker.datepicker({
@@ -41,21 +41,21 @@
             startDate: this.options.datepickerStartdate,
             todayHighlight: true,
         });
-        
+
         this.$dataLocker = this.$datePicker.next('input');
-        this.$datePicker.on('changeDate', $.proxy(this.onSelectDatePicker, this))	    
+        this.$datePicker.on('changeDate', $.proxy(this.onSelectDatePicker, this))
     }
-    
+
     Booking.prototype.initGuestPicker = function () {
         this.$el.delegate('[name="guest"]', 'change', $.proxy(this.onSelectGuestPicker, this));
         this.$guestPickerValue = this.$el.find('[name="guest"]').val();
 	}
-    
+
     Booking.prototype.initLocationPicker = function () {
         this.$el.delegate('[name="location"]', 'change', $.proxy(this.onSelectLocationPicker, this));
         this.$locationPickerValue = this.$el.find('[name="location"]').val();
     }
-    
+
     Booking.prototype.onSelectDatePicker = function(event) {
         var pickerDate = moment(event.date.toDateString())
         var lockerValue = pickerDate.format('YYYY-MM-DD')
@@ -63,35 +63,43 @@
         this.$dataLocker.val(lockerValue);
         this.onHtmlUpdate();
     }
-    
+
     Booking.prototype.onSelectGuestPicker = function(event) {
         var lockerValue = $(event.target).val();
         this.$guestPickerValue = lockerValue;
         this.onHtmlUpdate();
     }
- 
+
     Booking.prototype.onSelectLocationPicker = function(event) {
-        var lockerValue = $(event.target).val();
-        this.$locationPickerValue = lockerValue;
-        this.onHtmlUpdate();
+        location.href = location.pathname + '?location=' + event.target.value;
+        return;
     }
-           
+
     Booking.prototype.onHtmlUpdate = function() {
-        
+        var $indicatorContainer = this.$el.find('.progress-indicator-container')
+        $indicatorContainer.prepend('<div class="progress-indicator"></div>')
+        $indicatorContainer.addClass('is-loading')
+
 		jQuery.ajax(location.pathname + '?location=' + this.$locationPickerValue + '&date=' + this.$datePickerValue + '&guest=' + this.$guestPickerValue, {
 	         dataType: 'html'
 	    })
-	    .done(function(html) {    
-		    html = jQuery.parseHTML(html);
-		    html.forEach(function (node) {
-		        if (node.tagName && node.tagName.toUpperCase() == 'MAIN') {
-		            var newEl, currentEl;
-		            if ((newEl = node.querySelector('#ti-datepicker-options')) && (currentEl = document.querySelector('#ti-datepicker-options'))) {
-		                currentEl.innerHTML = newEl.innerHTML;
-		            }
-		        }
-		    });
-	    });	    
+	    .done($.proxy(this.onHtmlResponse, this));
+    }
+
+    Booking.prototype.onHtmlResponse = function(html) {
+	    html = jQuery.parseHTML(html);
+	    html.forEach(function (node) {
+	        if (node.tagName && node.tagName.toUpperCase() == 'MAIN') {
+	            var newEl, currentEl;
+	            if ((newEl = node.querySelector('#ti-datepicker-options')) && (currentEl = document.querySelector('#ti-datepicker-options'))) {
+	                currentEl.innerHTML = newEl.innerHTML;
+	            }
+	        }
+	    });
+
+        var $indicatorContainer = this.$el.find('.progress-indicator-container')
+        $indicatorContainer.find('.progress-indicator').remove()
+        $indicatorContainer.removeClass('is-loading')
     }
 
     Booking.DEFAULTS = {
