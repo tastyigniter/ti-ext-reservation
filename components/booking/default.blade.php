63cf948fceeba63b9107c60522d0837a77ad54ec
Sam Poyigi <sam@sampoyigi.com>
2020-11-07 11:36:49 +0000

    Resolve merge conflicts

--- components/booking/default.blade.php
+++ components/booking/default.blade.php
@@ -1,14 +1,13 @@
-<div
-    data-control="booking"
-    data-datepicker-startdate="{{ isset($nextOpen) ? $nextOpen->format('Y-m-d') : null }}"
-    data-datepicker-disableddaysofweek='@json($disabledDaysOfWeek ?? [])'
-    data-datepicker-disableddates='@json($disabledDates ?? [])'
->
+<div data-control="booking">
     @if ($__SELF__->pickerStep == 'info')
         @partial('@info')
 
         @partial('@booking_form')
 
+    @elseif ($__SELF__->pickerStep == 'timeslot')
+        <h1 class="h3">@lang('igniter.reservation::default.text_time_heading')</h1>
+
+        @partial('@timeslot')
     @else
         <h1 class="h3">@lang('igniter.reservation::default.text_heading')</h1>
 
--- components/booking/picker_form.blade.php
+++ components/booking/picker_form.blade.php
@@ -4,57 +4,84 @@
     'method' => 'GET',
 ]) !!}
 <input type="hidden" name="picker_step" value="2">
-<input type="hidden" name="location" value="{{ $__SELF__->location->getKey() }}">
+<input type="hidden" name="location" value="{{ optional($__SELF__->location)->getKey() }}">
 
-<div class="form-row align-items-center">
-    <div class="col-md-9 pr-md-4">
-        <div data-control="datepicker" data-date="{{ $__SELF__->getSelectedDate()->format('Y-m-d') }}"></div>
-        <input type="hidden" name="date" value="{{ $__SELF__->getSelectedDate()->format('Y-m-d') }}"/>
+<input type="hidden" name="picker_form" value="1">
+
+<div class="form-row">
+    <div class="col-sm-3 mb-3">
+        <label class="sr-only" for="location">
+            @lang('igniter.reservation::default.label_location')
+        </label>
+        <select
+                name="location"
+                id="location"
+                class="form-control"
+        >
+            <?php foreach ($__SELF__->getLocations() as $key => $value) { ?>
+            <option
+                    value="{{ $key }}"
+                    {!! set_select('location', $key) !!}
+            >{{ $value }}</option>
+            <?php } ?>
+        </select>
     </div>
-    <div class="col-md-3" id="ti-datepicker-options">
-        <div class="form-group pt-4">
-            <label for="noOfGuests">
-                @lang('igniter.reservation::default.label_guest_num')
-            </label>
-            <select
+    <div class="col-sm-2 mb-3">
+        <label class="sr-only" for="noOfGuests">
+            @lang('igniter.reservation::default.label_guest_num')
+        </label>
+        <select
                 name="guest"
                 id="noOfGuests"
                 class="form-control"
-            >
-                @foreach ($__SELF__->getGuestSizeOptions() as $key => $value)
-                    <option
-                        value="{{ $key }}"
-                        {!! set_select('guest', $key) !!}
-                    >{{ $value }}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="time">
-                @lang('igniter.reservation::default.label_time')
-            </label>
-            <select
+        >
+            <?php foreach ($__SELF__->getGuestSizeOptions() as $key => $value) { ?>
+            <option
+                    value="{{ $key }}"
+                    {!! set_select('guest', $key) !!}
+            >{{ $value }}</option>
+            <?php } ?>
+        </select>
+    </div>
+    <div class="col-sm-3 mb-3">
+        <label class="sr-only" for="date">
+            @lang('igniter.reservation::default.label_date')
+        </label>
+        <select
+                name="date"
+                id="date"
+                class="form-control"
+        >
+            <?php foreach ($__SELF__->getDatePickerOptions() as $date) { ?>
+            <option
+                    value="{{ $date->format('Y-m-d') }}"
+                    {!! set_select('date', $date->format('Y-m-d')) !!}
+            >{{ $date->isoFormat($bookingDateFormat) }}</option>
+            <?php } ?>
+        </select>
+    </div>
+    <div class="col-sm-2 mb-3">
+        <label class="sr-only" for="time">
+            @lang('igniter.reservation::default.label_time')
+        </label>
+        <select
                 name="time"
                 id="time"
                 class="form-control"
-            >
-                @foreach ($timeOptions as $key => $value)
-                    <option
-                        value="{{ $value->rawTime }}"
-                        {!! set_select('time', $value->rawTime) !!}
-                        {!! $value->fullyBooked ? 'disabled="disabled"' : '' !!}
-                    >{{ $value->time }}</option>
-                @endforeach
-            </select>
-        </div>
-        @if (count($timeOptions))
-            <div class="form-group">
-                <button
-                    type="submit"
-                    class="btn btn-primary btn-block"
-                >@lang('igniter.reservation::default.button_find_table')</button>
-            </div>
-        @endif
+        >
+            <?php foreach ($__SELF__->getTimePickerOptions() as $key => $value) { ?>
+            <option
+                    value="{{ $key }}"
+                    {!! set_select('time', $key) !!}
+            >{{ $value }}</option>
+            <?php } ?>
+        </select>
+    </div>
+    <div class="col-sm-2 mb-3">
+        <button
+                type="submit"
+                class="btn btn-primary btn-block"
+        >@lang('igniter.reservation::default.button_find_table')</button>
     </div>
 </div>
 <div class="form-row">
