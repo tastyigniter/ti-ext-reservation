e982e5b630509d0e8c8f2d89a4066fd6de98533d
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2021-10-05 19:37:45 +0100

    Use new reservation.isFullyBookedOn event to filter max guest per timeslot

--- listeners/MaxGuestSizePerTimeslotReached.php
+++ listeners/MaxGuestSizePerTimeslotReached.php
@@ -4,8 +4,6 @@ namespace Igniter\Reservation\Listeners;
 
 use Admin\Models\Reservations_model;
 use Carbon\Carbon;
-use Igniter\Flame\Exception\ApplicationException;
-use Igniter\Flame\Location\Models\AbstractLocation;
 use Igniter\Local\Facades\Location as LocationFacade;
 use Illuminate\Contracts\Events\Dispatcher;
 
@@ -15,39 +13,24 @@ class MaxGuestSizePerTimeslotReached
 
     public function subscribe(Dispatcher $dispatcher)
     {
-        $dispatcher->listen('igniter.workingSchedule.timeslotValid', __CLASS__.'@timeslotValid');
-
-        $dispatcher->listen('igniter.reservation.beforeSaveReservation', __CLASS__.'@beforeSaveReservation');
+        $dispatcher->listen('igniter.reservation.isFullyBookedOn', __CLASS__.'@isFullyBookedOn');
     }
 
-    public function timeslotValid($workingSchedule, $timeslot)
-    {
-        // Skip if the working schedule is not for opening
-        if ($workingSchedule->getType() != AbstractLocation::OPENING)
-            return;
-
-        if ($this->execute($timeslot))
-            return FALSE;
-    }
-
-    public function beforeSaveReservation($reservation, $data)
-    {
-        $dateTime = Carbon::createFromFormat('Y-m-d H:i', array_get($data, 'date').' '.array_get($data, 'time'));
-        if ($this->execute($dateTime, array_get($data, 'guest')))
-            throw new ApplicationException(lang('igniter.reservation::default.alert_fully_booked'));
-    }
-
-    protected function execute($timeslot, $guestNum = 0)
+    public function isFullyBookedOn($timeslot, $guestNum)
     {
         $locationModel = LocationFacade::current();
         if (!(bool)$locationModel->getOption('limit_reservations'))
+            return;
+
+        if (!$limitCount = (int)$locationModel->getOption('limit_guests_count', 20))
             return;
 
         $totalGuestNumOnThisDay = $this->getGuestNum($timeslot);
         if (!$totalGuestNumOnThisDay)
             return;
 
-        return ($totalGuestNumOnThisDay + $guestNum) >= (int)$locationModel->getOption('limit_guests_count', 20);
+        if (($totalGuestNumOnThisDay + $guestNum) > $limitCount)
+            return TRUE;
     }
 
     protected function getGuestNum($timeslot)
@@ -57,9 +40,8 @@ class MaxGuestSizePerTimeslotReached
         if (array_has(self::$reservationsCache, $date))
             return self::$reservationsCache[$date];
 
-        $locationModel = LocationFacade::current();
         $startTime = Carbon::parse($timeslot);
-        $endTime = Carbon::parse($timeslot)->addMinutes($locationModel->getReservationInterval())->subMinute();
+        $endTime = Carbon::parse($timeslot)->addMinutes(2);
 
         $guestNum = Reservations_model::where('location_id', LocationFacade::getId())
             ->whereBetweenReservationDateTime($startTime->toDateTimeString(), $endTime->toDateTimeString())
