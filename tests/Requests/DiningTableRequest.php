9fd2a884a0b7f9ff552f4784aa19f145231ed87b
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2023-07-08 23:30:52 +0100

    fix phpstan errors

---
+++ tests/Requests/DiningTableRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Requests;
+
+use Igniter\Reservation\Requests\DiningTableRequest;
+
+it('has required rule for inputs', function () {
+    expect('required')->toBeIn(array_get((new DiningTableRequest)->rules(), 'table_name'))
+        ->and('required')->toBeIn(array_get((new DiningTableRequest)->rules(), 'min_capacity'))
+        ->and('required')->toBeIn(array_get((new DiningTableRequest)->rules(), 'max_capacity'))
+        ->and('required')->toBeIn(array_get((new DiningTableRequest)->rules(), 'extra_capacity'))
+        ->and('required')->toBeIn(array_get((new DiningTableRequest)->rules(), 'priority'))
+        ->and('required')->toBeIn(array_get((new DiningTableRequest)->rules(), 'is_joinable'))
+        ->and('required')->toBeIn(array_get((new DiningTableRequest)->rules(), 'table_status'))
+        ->and('required')->toBeIn(array_get((new DiningTableRequest)->rules(), 'locations'));
+});
+
+it('has rules for table_name input', function () {
+    expect('between:2,255')->toBeIn(array_get((new DiningTableRequest)->rules(), 'table_name'));
+});
+
+it('has min character rule for min_capacity and max_capacity input', function () {
+    expect('min:1')->toBeIn(array_get((new DiningTableRequest)->rules(), 'min_capacity'))
+        ->and('min:1')->toBeIn(array_get((new DiningTableRequest)->rules(), 'max_capacity'));
+});
+
+it('has rules for max_capacity input', function () {
+    expect('lte:max_capacity')->toBeIn(array_get((new DiningTableRequest)->rules(), 'min_capacity'))
+        ->and('gte:min_capacity')->toBeIn(array_get((new DiningTableRequest)->rules(), 'max_capacity'));
+});
