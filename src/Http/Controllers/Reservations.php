0963910c3cebe41f847f2ff9957425456123aa13
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-01-18 00:50:17 +0000

    Avoid static method when throwing exception for better trace log
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/Http/Controllers/Reservations.php
+++ src/Http/Controllers/Reservations.php
@@ -115,7 +115,7 @@ class Reservations extends \Igniter\Admin\Classes\AdminController
     public function index_onDelete()
     {
         throw_unless($this->authorize('Admin.DeleteReservations'),
-            FlashException::error(lang('igniter::admin.alert_user_restricted'))
+            new FlashException(lang('igniter::admin.alert_user_restricted'))
         );
 
         return $this->asExtension(\Igniter\Admin\Http\Actions\ListController::class)->index_onDelete();
@@ -139,7 +139,7 @@ class Reservations extends \Igniter\Admin\Classes\AdminController
     public function edit_onDelete()
     {
         throw_unless($this->authorize('Admin.DeleteReservations'),
-            FlashException::error(lang('igniter::admin.alert_user_restricted'))
+            new FlashException(lang('igniter::admin.alert_user_restricted'))
         );
 
         return $this->asExtension(\Igniter\Admin\Http\Actions\FormController::class)->edit_onDelete();
@@ -155,7 +155,7 @@ class Reservations extends \Igniter\Admin\Classes\AdminController
     public function calendarUpdateEvent($eventId, $startAt, $endAt)
     {
         throw_unless($reservation = Reservation::find($eventId),
-            FlashException::error(lang('igniter.reservation::default.alert_no_reservation_found'))
+            new FlashException(lang('igniter.reservation::default.alert_no_reservation_found'))
         );
 
         $startAt = make_carbon($startAt);
